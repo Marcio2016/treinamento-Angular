1553ac6907a83fa0b1c422d13e5a01581d87340c
Marcio2016 <marcio.tadeu2@gmail.com>
2020-05-05 19:53:21 -0300

    Interpolação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { InterpolacaoComponent } from './interpolacao/interpolacao.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InterpolacaoComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/interpolacao/interpolacao.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-interpolacao',
+  templateUrl: './interpolacao.component.html',
+  styleUrls: ['./interpolacao.component.css']
+})
+export class InterpolacaoComponent {
+
+  nome = ['fullStack Angular SpringRest']
+
+}
